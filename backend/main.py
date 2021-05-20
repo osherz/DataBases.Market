b74a796a15266600e1a5c474f693c6ff09b007b6
Osher Zakin <osherz92@gmail.com>
2021-05-20 17:04:15 +0300

    Example of fetching from db

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,21 @@ from flask import Flask, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_DB'] = ''
-app.config['MYSQL_USERNAME'] = ''
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_HOST'] = 'freedb.tech'
+app.config['MYSQL_DB'] = 'freedbtech_market'
+app.config['MYSQL_USER'] = 'freedbtech_osher'
+app.config['MYSQL_PASSWORD'] = '123456'
 mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
-    return 'Success'
+    query = "select * from employees"
+    with mysql.connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchall()
+    return jsonify(result)
 
 
-if __name__ == '__main__':
-    app.run('localhost', 5000)
+
+app.run('localhost', 5000)
