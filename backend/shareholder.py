488b1ab61e08d27b016fba6c0aad41dc4dfff392
ron hodadedi <ronchaim318@gmail.com>
2021-05-25 21:57:55 +0300

    shareholder

---
+++ backend/shareholder.py
@@ -0,0 +1,16 @@
+from flask_mysqldb import MySQL
+import utils
+
+
+def insert(mysql: MySQL, ID, STOCK, EMAIL, NAME):
+    query = f"insert into shareholder values({ID},{STOCK},{EMAIL},{NAME})"
+    utils.execute_action(mysql, query)
+
+
+def update(mysql: MySQL, STOCK, EMAIL, NAME):
+    query = f"update product_in_branch " \
+            f"set" \
+            f"STOCK{STOCK}" \
+            f"EMAIL{EMAIL}" \
+            f"NAME{NAME}"
+    utils.execute_action(mysql, query)
