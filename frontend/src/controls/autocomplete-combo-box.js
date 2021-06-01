1374f55a5b6051fdd3d57191906765a085d42fcb
Osher Zakin <osherz92@gmail.com>
2021-06-01 20:23:36 +0300

    Add option to decide labal for AutoCompleteComboBox component

--- frontend/src/controls/autocomplete-combo-box.js
+++ frontend/src/controls/autocomplete-combo-box.js
@@ -3,21 +3,20 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 
 
-export default function AutocompleteComboBox({value, handleValueChanged, tableName, columnToShow}) {
+export default function AutocompleteComboBox({value, handleValueChanged, tableName, columnToShow, label=''}) {
     const [data, setData] = useState([]);
     useEffect(() => {
         fetch(`/${tableName}/select`)
             .then(result => result.json())
             .then(json => setData(json['data']));
     }, [tableName]);
-
     return (
         <Autocomplete
             options={data}
             getOptionLabel={option => option[columnToShow]}
             value={value}
             onChange={(event, newValue) => handleValueChanged(newValue)}
-            renderInput={(params) => <TextField {...params} label={'Choose ' + tableName} variant="outlined" />}
+            renderInput={(params) => <TextField {...params} label={'Choose ' + (label.length > 0 ? label : tableName)} variant="outlined" />}
             />
     )
 }
