0153f5638b0e40fd27546f1af169b6f71fc97ef9
yaron <yarongerstel@windowslive.com>
2021-06-03 17:38:16 +0300

    3.6

--- backend/querys/biggest_shareholders.sql
+++ backend/querys/biggest_shareholders.sql
@@ -1,6 +1,4 @@
-# To knows who our biggest manufaturer are, and if it’s too much:
-SELECT Manufacturer.id, Manufacturer.name, me.expenses
-FROM Manufacturer
-         join manufacturer_expenses me
-             on Manufacturer.id = me.manufacturer_id
-WHERE me.expenses > 100000
+SELECT *
+FROM shareholder
+ORDER BY stock DESC
+limit 1
--- backend/querys/our_biggest_manufaturer.sql
+++ backend/querys/our_biggest_manufaturer.sql
@@ -1,3 +1,6 @@
-SELECT ID,SALARY
-FROM employees
-WHERE SALARY>10000
+# To knows who our biggest manufaturer are, and if it’s too much:
+SELECT Manufacturer.id, Manufacturer.name, me.expenses
+FROM Manufacturer
+         join manufacturer_expenses me
+             on Manufacturer.id = me.manufacturer_id
+WHERE me.expenses > 100000
